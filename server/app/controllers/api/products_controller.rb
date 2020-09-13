c83195525704a0059ec63817adf9022408c5cfc4
Mona Parnian <m.parnian.eng@gmail.com>
2020-09-13 22:26:06 +1000

    took userid out of product controller then product are not belongs to specific user

--- server/app/controllers/api/products_controller.rb
+++ server/app/controllers/api/products_controller.rb
@@ -1,34 +1,47 @@
 class Api::ProductsController <ApplicationController
   before_action :user_exists
+  before_action :user_admin, except: :index
 
   def index
-    render json: User.find(@user_id).products.all
+    render json: Product.all.order(:id)
   end
 
+
   def create
-    if @user.role != 'admin'
-      render json: {message: 'Unauthorized user'}, status: :unauthorized
+    new_product = Product.create(products_params)
+    if new_product.valid?
+      render json: new_product
     else
-      render json: User.find(@user_id).products.create(products_params)
+      render json: { errors: new_product.errors.full_messages }, status: :not_acceptable
     end
+
   end
 
   def show
-    render json: User.find(@user_id).products.find(params[:id])
+    render json: Product.find(params[:id])
   end
 
   def update
-    product = User.find(@user_id).products.find(params[:id])
-    render json: product.update(products_params)
+    find_product = Product.find(params[:id])
+    if find_product.valid?
+      update_product = find_product.update(products_params)
+      render json: update_product
+    else
+      render json: { errors: product_code.errors.full_messages }, status: :not_acceptable
+    end
   end
 
   def destroy
-    render json: User.find(@user_id).products.destroy(params[:id])
+    render json: Product.destroy(params[:id])
+  end
+
+  def show_products
+    render json: Product.reselect(:id, :product_code, :product_description)
   end
 
   def details
     p params[:id]
-    render json: User.find(@user_id).products.find(params[:product_code])
+    render json: Product.find(params[:product_code])
   end
 
 
@@ -37,8 +50,8 @@ class Api::ProductsController <ApplicationController
   def products_params
     params.required(:product).permit(:product_code,
                                      :product_description,
-                                     :unit_of_measurement,
-                                     :planning_fence_rule,
+                                     :UOM,
+                                     :PFR,
                                      :MOQ,
                                      :safety_stock,
                                      :supplier_name,
