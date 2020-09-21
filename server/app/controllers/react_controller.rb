e137f0b820d29a1555068d304d266cfd012c1c08
Mona Parnian <m.parnian.eng@gmail.com>
2020-09-21 11:58:27 +1000

    reformat react-controller and added a comments in it

--- server/app/controllers/react_controller.rb
+++ server/app/controllers/react_controller.rb
@@ -1,7 +1,8 @@
 class ReactController <ActionController::Base
 
+  #below line render a file, in all our previous controller we render json but here is a file
   def react_app
-    render :file => "#{Rails.root}/public/index.html", content_type: 'text/html', layout: false, formats: :html
+    render file: "#{Rails.root}/public/index.html", content_type: 'text/html', layout: false, formats: :html
   end
 
 end
